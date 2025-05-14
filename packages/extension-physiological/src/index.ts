8f26412795791da33e69dc12aaedcfc4bf6f699c
Constance Bainbridge <connie@Constances-MacBook-Pro.local>
2025-05-14 14:39:26 -0400

    Updated data structure to be able to return after API call

--- packages/extension-physiological/src/index.ts
+++ packages/extension-physiological/src/index.ts
@@ -51,48 +51,27 @@ class PhysiologicalExtension implements JsPsychExtension {
   on_load = (): void => {};
 
   on_finish = (): { [key: string]: any } => {
+    var profile;
     var hr;
+    return new Promise((resolve, reject) => {
+      fetch("http://localhost:3000/fitbit-profile")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Fitbit data Ohdhdhdksjdfo:", data);
+          profile = data;
 
-    // fetch(
-    //   "https://api.fitbit.com/1/user/-/activities/heart/date/2025-05-13/2025-05-13/1sec/time/08:00/08:30.json",
-    //   {
-    //     method: "GET",
-    //     headers: {
-    //       Accept: "application/json",
-    //       Authorization:
-    //         "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyM1FITlgiLCJzdWIiOiJDTFo4WFQiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJyc29jIHJlY2cgcnNldCByaXJuIHJveHkgcm51dCBycHJvIHJzbGUgcmNmIHJhY3QgcnJlcyBybG9jIHJ3ZWkgcmhyIHJ0ZW0iLCJleHAiOjE3NDcyNjQyODAsImlhdCI6MTc0NzIzNTQ4MH0.WT1nWy5MPFDpqhn-rEdODkHKCuzJNR2KIbt4hMlEUTs",
-    //     },
-    //   }
-    // )
-    //   .then((response) => {
-    //     if (!response.ok) {
-    //       throw new Error(`HTTP error! status: ${response.status}`);
-    //     }
-    //     return response.json();
-    //   })
-    //   .then((data) => {
-    //     console.log("Heart Rate Data:", data);
-    //     hr = data;
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error fetching heart rate data:", error);
-    //   });
-
-    fetch("http://localhost:3000/fitbit-profile")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Fitbit data:", data);
-        // You can now use the Fitbit data inside your jsPsych timeline
-      })
-      .catch((error) => {
-        console.error("Error fetching Fitbit data:", error);
-      });
-
-    return {
-      data1: 99, // Make sure this type and name matches the information for data1 in the data object contained within the info const.
-      data2: "hello world!", // Make sure this type and name matches the information for data2 in the data object contained within the info const.
-      heartrate: hr,
-    };
+          // Once data is fetched, resolve the Promise with the return object
+          resolve({
+            data1: 99, // Ensure these match the expected return format
+            data2: "hello world!",
+            heartrate: hr,
+            profile: profile,
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching Fitbit data:", error);
+        });
+    });
   };
 }
 
